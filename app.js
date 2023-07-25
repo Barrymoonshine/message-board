75ca7a215d137b2a852c53aa18247de2d272ed45
Nick Sheward <nickshwrd@gmail.com>
2023-07-25 15:18:06 +0100

    Created new view with form inputs, and inputs being posted onto index view using post method

--- app.js
+++ app.js
@@ -12,19 +12,36 @@ app.use(express.static('public'));
 // Register View Engine (EJS)
 app.set('view engine', 'ejs');
 
-// Render a view
+const messages = [
+  {
+    text: 'Hi there!',
+    user: 'Amando',
+    added: new Date(),
+  },
+  {
+    text: 'Hello World!',
+    user: 'Charles',
+    added: new Date(),
+  },
+];
+
+// Render views
 app.get('/', (req, res) => {
-  const messages = [
-    {
-      text: 'Hi there!',
-      user: 'Amando',
-      added: new Date(),
-    },
-    {
-      text: 'Hello World!',
-      user: 'Charles',
-      added: new Date(),
-    },
-  ];
-  res.render('index', { messages });
+  res.render('index', { title: 'Messages', messages });
+});
+
+// Encode Post data on the req object
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/new', (req, res) => {
+  res.render('new', { title: 'New Message' });
+});
+
+app.post('/new', (req, res) => {
+  messages.push({
+    text: req.body.message,
+    user: req.body.userName,
+    added: new Date(),
+  });
+  res.redirect('/');
 });
