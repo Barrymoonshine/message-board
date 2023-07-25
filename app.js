d36f2cafc0c0772d34cf79e9d23e7557ce1bd13f
Nick Sheward <nickshwrd@gmail.com>
2023-07-25 14:05:54 +0100

    Dummy messages being printed on index using EJS

--- app.js
+++ app.js
@@ -14,5 +14,17 @@ app.set('view engine', 'ejs');
 
 // Render a view
 app.get('/', (req, res) => {
-  res.render('index');
+  const messages = [
+    {
+      text: 'Hi there!',
+      user: 'Amando',
+      added: new Date(),
+    },
+    {
+      text: 'Hello World!',
+      user: 'Charles',
+      added: new Date(),
+    },
+  ];
+  res.render('index', { messages });
 });
